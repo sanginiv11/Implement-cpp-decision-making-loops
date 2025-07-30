9478a3ebc38a33842e5049af81f920902d46c42d
Sangini Verma <sanginiv11@gmail.com>
2025-07-30 23:15:04 +0530

    Add files via upload

---
+++ leftalignednumbertriangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a=1;
+    for (int i=0;i<=3;i++){
+        for (int j=0;j<=i;j++){
+          cout<<a++<<" ";}
+          cout<<endl;
+        }
+        return 0;
+    }
