2e0b6cc4887086141d6be4d19a4a45625bb4b4f0
Sangini Verma <sanginiv11@gmail.com>
2025-07-31 00:20:41 +0530

    Add files via upload

---
+++ passwordauthenticate.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string password, input;
+    password = "Sangini"; // Set your password here
+
+    cout << "Enter password: ";
+    cin >> input;
+
+    if(input == password) {
+        cout << "Access granted." << endl;
+    } else {
+        cout << "Access denied. Incorrect password." << endl;
+    }
+    return 0;
+}
