8781eccd4b9f60cd62ae9246848133e6b5d10095
Sangini Verma <sanginiv11@gmail.com>
2025-07-31 14:54:09 +0530

    Update hourglassstarpattern.cpp

--- hourglassstarpattern.cpp
+++ hourglassstarpattern.cpp
@@ -6,26 +6,19 @@ int main() {
     cout << "Enter the number of rows (half of the hourglass): ";
     cin >> n;
 
-    // Upper half of hourglass
     for (int i = 0; i < n; i++) {
-        // Spaces
         for (int j = 0; j < i; j++) {
             cout << " ";
         }
-        // Stars
         for (int j = 0; j < 2 * (n - i) - 1; j++) {
             cout << "*";
         }
         cout << endl;
     }
-
-    // Lower half of hourglass
     for (int i = 1; i <= n; i++) {
-        // Spaces
         for (int j = 0; j < n - i; j++) {
             cout << " ";
         }
-        // Stars
         for (int j = 0; j < 2 * i - 1; j++) {
             cout << "*";
         }
@@ -34,3 +27,16 @@ int main() {
 
     return 0;
 }
+/* output
+Enter the number of rows (half of the hourglass): 5
+*********
+ *******
+  *****
+   ***
+    *
+    *
+   ***
+  *****
+ *******
+********* 
+    */
